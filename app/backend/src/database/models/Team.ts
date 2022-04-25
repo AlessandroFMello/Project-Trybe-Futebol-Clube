bf9eaa749cf4f6332b70f17c4c5aaae433fd0410
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-04-25 16:02:49 -0300

    feat: configures teams models/migrations

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,26 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class Team extends Model {
+  id: number;
+
+  teamName: string;
+}
+
+Team.init({
+  id: {
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: DataTypes.STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
