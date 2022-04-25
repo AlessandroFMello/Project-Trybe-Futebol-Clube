addff760dbdfd1708abffd34d1e27707427eb35b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-04-25 16:03:26 -0300

    feat: configures matches models/migrations

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,49 @@
+import { DataTypes, Model } from 'sequelize/types';
+import db from '.';
+
+export default class Match extends Model {
+  id: number;
+
+  homeTeam: number;
+
+  homeTeamId: number;
+
+  awayTeam: number;
+
+  awayTeamGoals: number;
+
+  inProgress: boolean;
+}
+Match.init({
+  id: {
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: DataTypes.INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: DataTypes.BOOLEAN,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
