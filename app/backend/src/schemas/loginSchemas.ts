d4436ade3078b727a735cdefb5ebd92508b367ff
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-04-27 15:33:39 -0300

    fix: changes joi schema

--- app/backend/src/schemas/loginSchemas.ts
+++ app/backend/src/schemas/loginSchemas.ts
@@ -1,17 +1,15 @@
 import Joi, { ObjectSchema } from 'joi';
 
-const error401 = '401|Incorrect email or password';
-
 const loginSchemas: ObjectSchema = Joi.object({
   email: Joi.string().required().messages({
     'any.required': '400|All fields must be filled',
-    'string.base': error401,
+    'string.base': '401|Incorrect email or password',
   }),
 
   password: Joi.string().min(7).required().messages({
     'any.required': '400|All fields must be filled',
-    'string.base': error401,
-    'string.min': error401,
+    'string.base': '401|Incorrect email or password',
+    'string.min': '401|String must be at least 7 characters long',
   }),
 });
 
