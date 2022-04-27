7260ea188f58131f2863befd94421f0d4eddead4
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-04-27 14:18:50 -0300

    feat: creates login validation middleware

---
+++ app/backend/src/middlewares/loginMiddleware.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+import loginSchemas from '../schemas/loginSchemas';
+
+const loginMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  const { error } = loginSchemas.validate({ email, password });
+
+  if (error) {
+    const [code, message] = error.message.split('|');
+
+    return res.status(Number(code)).json({ message });
+  }
+
+  return next();
+};
+
+export default loginMiddleware;
