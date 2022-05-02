d4496475ac4fc4cf0764d1f24fa4c757a4632c79
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-02 19:48:29 -0300

    feat: creates teams router

---
+++ app/backend/src/routes/teamsRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import TeamsController from '../controllers/teamsController';
+
+const router = Router();
+
+const teamsController = new TeamsController();
+
+router.get('/', teamsController.getAll);
+
+export default router;
