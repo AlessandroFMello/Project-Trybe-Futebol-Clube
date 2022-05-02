21cc95564d04f7bc851ce2aac59868ad4018927a
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-02 16:45:47 -0300

    feat: creates loginController validate function

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 import LoginService from '../services/loginService';
+import TokenGenerator from '../tokenGenerator';
 
 export default class LoginController {
   private loginService: LoginService;
@@ -18,5 +20,21 @@ export default class LoginController {
     }
 
     return res.status(code).json(user);
+  };
+
+  public validation = async (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(400).json({ message: 'Unauthorized' });
+    }
+
+    const tokenGenerator = new TokenGenerator();
+
+    const decodedUser = await tokenGenerator.decodeToken(authorization) as JwtPayload;
+
+    console.log(decodedUser);
+
+    return res.status(200).json(decodedUser.role);
   };
 }
