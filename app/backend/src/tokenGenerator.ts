757ddebf5be0288ac51d33260e6f4b518339e384
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-02 16:46:09 -0300

    feat: creates TokenGenerator decode function

--- app/backend/src/tokenGenerator.ts
+++ app/backend/src/tokenGenerator.ts
@@ -5,9 +5,14 @@ import TokenShape from './interfaces/tokenShape';
 export default class TokenGenerator {
   public tokenGenerator = async (
     { id, username, email, role }: TokenShape,
-  ) => jwt.sign(
+  ): Promise<string> => jwt.sign(
     { id, username, email, role },
     await readFile('jwt.evaluation.key', 'utf8'),
     { expiresIn: '7d', algorithm: 'HS256' },
+  );
+
+  public decodeToken = async (token: string) => jwt.verify(
+    token,
+    await readFile('jwt.evaluation.key', 'utf8'),
   );
 }
