497795d025e44dd461a5c383a667f22c78c03f22
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-03 13:54:48 -0300

    feat: Creates middleware to validate token

---
+++ app/backend/src/middlewares/tokenValidation.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+// import { JwtPayload } from 'jsonwebtoken';
+// import TokenGenerator from '../tokenGenerator';
+
+const tokenValidation = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+      return res.status(400).json({ message: 'Unauthorized' });
+    }
+
+    // const tokenGenerator = new TokenGenerator();
+
+    // const decodedUser = tokenGenerator.decodeToken(authorization) as JwtPayload;
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: 'Unauthorized' });
+  }
+};
+
+export default tokenValidation;
