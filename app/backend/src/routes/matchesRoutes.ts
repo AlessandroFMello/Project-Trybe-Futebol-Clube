2ca18522e55776c3489c60dd4e7f1a0ac30377fe
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-03 13:55:10 -0300

    feat: Creates POST route to create a match

--- app/backend/src/routes/matchesRoutes.ts
+++ app/backend/src/routes/matchesRoutes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import tokenValidation from '../middlewares/tokenValidation';
 import MatchesController from '../controllers/matchesController';
 
 const router = Router();
@@ -8,5 +9,7 @@ const matchesController = new MatchesController();
 router.get('/', matchesController.getAll);
 
 router.get('/:id', matchesController.getById);
+
+router.post('/', tokenValidation, matchesController.create);
 
 export default router;
