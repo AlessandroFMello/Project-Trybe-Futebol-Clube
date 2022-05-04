23ed6134ecc82cca0b2b70af7497ea7ffd368c9b
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-04 00:37:40 -0300

    feat: Creates leaderboard/away route

--- app/backend/src/routes/leaderboardRoutes.ts
+++ app/backend/src/routes/leaderboardRoutes.ts
@@ -7,7 +7,7 @@ const leaderboardController = new LeaderboardController();
 
 router.get('/home', leaderboardController.getHome);
 
-// router.get('/away', leaderboardController.getAway);
+router.get('/away', leaderboardController.getAway);
 
 // router.get('/', leaderboardController.getAllGames);
 export default router;
