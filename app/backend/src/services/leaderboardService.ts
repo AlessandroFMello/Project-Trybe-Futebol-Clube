1f8c1f2400841f495a5e0ae6548e04e4e9713215
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-04 00:39:54 -0300

    feat: Creates getAway method in leaderboardService

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -37,6 +37,31 @@ export default class LeaderboardService {
     return { code: 200, leaderboard: this.compareTeams(results) };
   }
 
+  public async getAway() {
+    const teams = await this.getAllTeams();
+    const matches = await this.getAllMatches();
+
+    const results: TeamMatchesResults[] = [];
+    teams.forEach((team) => {
+      const filteredMatches = matches
+        .filter(
+          (filteredMatch) => filteredMatch.inProgress === false
+      && filteredMatch.awayTeam === team.id,
+        );
+      const mappedMatches = filteredMatches
+        .map((mappedMatch) => ({
+          goalsFavor: mappedMatch.awayTeamGoals,
+          goalsOwn: mappedMatch.homeTeamGoals,
+        }));
+
+      const teamResults = new TeamMatchesResults(team.teamName, mappedMatches);
+
+      results.push(teamResults);
+    });
+
+    return { code: 200, leaderboard: this.compareTeams(results) };
+  }
+
   private compareTeams = (teams: TeamMatchesResults[]) => teams.sort((a, b) => {
     if (a.totalPoints < b.totalPoints) return 1;
     if (a.totalPoints > b.totalPoints) return -1;
