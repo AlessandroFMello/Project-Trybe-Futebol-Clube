d0890bb8908edbdfe29fd9de4e38edfcb8c1e5b2
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-04 00:08:36 -0300

    feat: creates getHome method in leaderboardController

---
+++ app/backend/src/controllers/leaderboardController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import LeaderboardService from '../services/leaderboardService';
+
+export default class LeaderboardController {
+  private leaderboardService: LeaderboardService;
+
+  constructor() {
+    this.leaderboardService = new LeaderboardService();
+  }
+
+  public getHome = async (_req: Request, res: Response) => {
+    const { code, leaderboard } = await this.leaderboardService.getHome();
+
+    return res.status(code).json(leaderboard);
+  };
+
+}
